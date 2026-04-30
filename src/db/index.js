fb765628bd4f770098d7426ddeb8c0eb51f14238
vikramlota <viccckkylota@gmail.com>
2026-04-30 18:40:29 +0530

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`\nMongoDB connected!! DB HOST: ${connectionInstance.connection.host} | DB: ${connectionInstance.connection.name}`);
-        return connectionInstance;
-    } catch (error) {
-        console.error("MONGODB connection error:", error.message);
-        throw error;
-    }
-};
+mongoose.connect(process.env.MONGODB_URI, {
+  dbName: "samarthacademy"
+});
 
 module.exports = connectDB;
