d04eda8e2cafa2ccc4478fe6ae3d7b9201e5cff3
vikramlota <viccckkylota@gmail.com>
2026-04-30 18:23:02 +0530

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,23 +1,8 @@
 const mongoose = require("mongoose");
-const DB_NAME = process.env.DB_NAME || 'samarthacademy';
 
 const connectDB = async () => {
     try {
-        let uri = process.env.MONGODB_URI;
-
-        // Safety net: if URI has no DB name (ends with / or just has query params), inject DB_NAME
-        if (process.env.DB_NAME) {
-            const hasDbName = /mongodb(\+srv)?:\/\/[^/]+\/[^/?]+/.test(uri);
-            if (!hasDbName) {
-                uri = uri.replace(/(\?.*)$/, `/${process.env.DB_NAME}$1`);
-                if (!uri.includes(`/${process.env.DB_NAME}`)) {
-                    uri = `${uri.replace(/\/?$/, '')}/${process.env.DB_NAME}`;
-                }
-            }
-        }
-
-        const connectionInstance = await mongoose.connect(uri);
-
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`\nMongoDB connected!! DB HOST: ${connectionInstance.connection.host} | DB: ${connectionInstance.connection.name}`);
         return connectionInstance;
     } catch (error) {
