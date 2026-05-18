e8ee2395482f8029888621fd79416b2bd38697c4
vikramlota <viccckkylota@gmail.com>
2026-05-18 17:33:57 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -89,8 +89,8 @@ app.use(async (req, res, next) => {
 
 
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true,limit: "16kb"}))
+app.use(express.json({limit: "10mb"}))
+app.use(express.urlencoded({extended: true,limit: "10mb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
